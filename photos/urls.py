93e84e22fe90cea873a5a2e9274a83c7f2b8070f
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-13 16:12:51 +0300

    add url path to welcome page

--- photos/urls.py
+++ photos/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns =[
-    url(r'^$', views.home, name = 'home'),
-    url(r'^photos/(\d+)', views.photo, name='photo_detail')
+    url(r'^$', views.welcome, name = 'welcome'),
+    url(r'^main/', views.main, name = 'main'),
+    url(r'^photos/(\d+)', views.photo, name='photo_detail'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
