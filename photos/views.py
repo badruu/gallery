ff9fb74c2fff58ad7127d9b4550467d9dca39400
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-13 16:13:20 +0300

    add welcome view fxn

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Photo
 
-def home(request):
+def welcome(request):
+    return render(request, 'photos/welcome.html', {'title': 'welcome'})
+
+def main(request):
     context={
         'photos':Photo.objects.all()
     }
@@ -10,3 +13,16 @@ def home(request):
 def photo(request, photo_id):
     photo = Photo.objects.get(id = photo_id)
     return render(request, 'photos/photos.html', {"photo":photo})
+
+def search_results(request):
+
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_articles = Photo.search_by_image_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photos/search.html',{"message":message,"photos": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photos/search.html',{"message":message})
