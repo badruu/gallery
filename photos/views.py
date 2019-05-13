8a08413a82ad520f8d702d55f53c78919099071e
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-13 07:46:58 +0300

    add view function for a single photo

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Photo
 
-# Create your views here.
+def home(request):
+    context={
+        'photos':Photo.objects.all()
+    }
+    return render(request, 'photos/home.html',context)
+
+def photo(request, photo_id):
+    photo = Photo.objects.get(id = photo_id)
+    return render(request, 'photos/photos.html', {"photo":photo})
