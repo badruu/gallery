a56cafe39d7fa8bdef6d898d47a4d0a624057c00
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-13 11:01:14 +0300

    change Category to categoty

--- photos/models.py
+++ photos/models.py
@@ -1,18 +1,29 @@
 from django.db import models
+import datetime as dt
+
+
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    city = models.CharField(default = 'Nairobi', max_length = 70)
+    country = models.CharField(default = 'Kenya', max_length = 70)
+    def __str__(self):
+        return self.city+" "+self.country
+
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
 
 class Photo(models.Model):
-    image_name = models.CharField(default = 'IMG', max_length = 30)
+    image_name = models.CharField(default = 'Image', max_length = 30)
     image = models.ImageField(default = 'default.jpg', upload_to = 'images')
     image_desc = models.CharField(max_length = 70)
-    # location = models.ManyToManyField(location)
-    # pub_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
     def __str__(self):
         return self.image_desc
 
     def save_photo(self):
         self.save()
-
-# class Location(models.Model):
-#     place = models.CharField(max_length = 70)
-#     def __str__(self):
-#         return self.place
